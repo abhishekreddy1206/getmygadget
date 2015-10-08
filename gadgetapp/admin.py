321b8331f6a5eb7eb71c517f8857f51e042179da
Abhishek Yeruva <abhishek.yeruva@ldalmacr077g3qd.dt.inc>
2015-10-08 12:00:20 -0700

    models and admin

--- gadgetapp/admin.py
+++ gadgetapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django_mptt_admin.admin import DjangoMpttAdmin
+from models import *
 
 # Register your models here.
+
+class InventoryAdmin(DjangoMpttAdmin):
+    pass
+
+admin.site.register(DTUser)
+admin.site.register(Inventory, InventoryAdmin)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
--- gadgetapp/models.py
+++ gadgetapp/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
+from mptt.models import MPTTModel, TreeForeignKey
+import datetime
 
 # Create your models here.
+
+class DTUser(models.Model):
+    USER_CHOICES = (
+        ('R', 'Requester'),
+        ('A', 'Approver'),
+    )
+    user = models.OneToOneField(User)
+    user_type = models.CharField(choices=USER_CHOICES, default='R', max_length=50)
+
+    class Meta:
+        verbose_name = "DT User"
+        verbose_name_plural = "DT Users"
+
+    def __unicode__(self):
+        return self.user.username
+
+class Inventory(MPTTModel):
+    name = models.CharField(max_length=50)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
+    description = models.CharField(blank=True, max_length=500)
+    price = models.FloatField(default = 0)
+
+    class Meta:
+        verbose_name = "Inventory"
+        verbose_name_plural = "Inventories"
+
+    def __unicode__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('R', 'Received'),
+        ('A', 'Approved'),
+        ('RE', 'Rejected'),
+        ('D', 'Dispatched')
+    )
+    user = models.ForeignKey(User, related_name='requester')
+    approver = models.ForeignKey(User, related_name='approver', blank=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=STATUS_CHOICES, default='R', max_length=50)
+
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
+
+    def __unicode__(self):
+        return self.id
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order)
+    inventory = models.ForeignKey(Inventory)
+    quantity = models.IntegerField(default = 1)
+
+    class Meta:
+        verbose_name = "Order Detail"
+        verbose_name_plural = "Order Details"
+
+    def __unicode__(self):
+        return self.id
